3447207595a25f0c6a0e0053939febe418f5139c
Antônio Ivo da Silva Gomes <144092212+antonio-ivo@users.noreply.github.com>
2025-03-29 10:48:46 -0300

    create build_project.ps1 script to build main.c
    - create main.c file

---
+++ build_project.ps1
@@ -0,0 +1,19 @@
+
+#
+#  MIT No Attribution
+#  Full text: https://opensource.org/licenses/MIT-0
+#
+#  Copyright 2025 Antônio Ivo da Silva Gomes
+#
+
+$cc = 'gcc'
+
+$include_flags = '-ISDL/include'
+$library_folder_path = '-Lvendor'
+$library_flags = '-lSDL3'
+
+$files = 'main.c'
+$executable_name = 'o.exe'
+
+$command = "$cc -o $executable_name $files $include_flags $library_folder_path $library_flags"
+iex $command
