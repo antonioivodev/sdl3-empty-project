1efc41d6178f911cbc27b90a51bf3bfb51dddc33
Antônio Ivo da Silva Gomes <144092212+antonioivodev@users.noreply.github.com>
2025-04-23 22:26:08 -0300

    update build_project.ps1, file executable name

--- build_project.ps1
+++ build_project.ps1
@@ -13,7 +13,7 @@ $library_folder_path = '-Lvendor'
 $library_flags = '-lSDL3'
 
 $files = 'main.c'
-$executable_name = 'o.exe'
+$executable_name = 'a.exe'
 
 $command = "$cc -o $executable_name $files $include_flags $library_folder_path $library_flags"
 iex $command
